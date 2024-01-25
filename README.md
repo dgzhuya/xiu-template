9fea628b4b9eaf733fb64a7d7bc8a7aa22c3bdbf
dgzhuya <chenbiuxiu@gmail.com>
2024-01-25 21:44:15 +0800

    🎉 init: 初始化模板插件

---
+++ README.md
@@ -0,0 +1,3 @@
+# xiu-template README
+
+### 配合`@biuxiu/template`实现模板工具类型提示功能
---
+++ src/util.ts
@@ -0,0 +1,42 @@
+import { join } from 'path'
+import * as vscode from 'vscode'
+
+const findCurrentFolder = (folders: string[], activeFile: string) => {
+	if (folders.length === 1) {
+		return folders[0]
+	}
+	const activePath = activeFile
+		.split('/')
+		.filter(f => f)
+		.map((_, i, arr) => (i === 0 ? '' : '/' + arr.slice(0, i).join('/')))
+		.filter(f => f)
+	const folder = folders.filter(f => activePath.includes(f))
+	if (folder.length === 1) {
+		return folder[0]
+	}
+}
+
+export const findTsBtpl = (
+	activeFile: string,
+	workFolders: readonly vscode.WorkspaceFolder[]
+) => {
+	if (workFolders) {
+		const curPath = findCurrentFolder(
+			workFolders.map(w => w.uri.fsPath),
+			activeFile
+		)
+		if (!curPath) {
+			vscode.window.showInformationMessage('当前编辑文件不在项目中')
+			return
+		}
+		if (!/\/sources\/[a-zA-Z-_0-9]*\.btpl/.test(activeFile)) {
+			console.log('activeFile: ', activeFile)
+			vscode.window.showInformationMessage('当前文件未在工作区')
+			return
+		}
+		return {
+			tsFilePath: join(curPath, 'btpl-env.d.ts'),
+			name: activeFile.split('/').slice(-1).join('').split('.')[0]
+		}
+	}
+}
